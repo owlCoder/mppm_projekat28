581c752a76c4ec22be7e87036f12b28d9b778ad4
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2023-12-29 20:39:27 +0100

    Bug fixes

--- MVVM3/ViewModel/GetRelatedValuesViewModel.cs
+++ MVVM3/ViewModel/GetRelatedValuesViewModel.cs
@@ -15,13 +15,14 @@ namespace MVVM3.ViewModel
     {
         public MyICommand ClearProperties { get; set; }
         public MyICommand ResetAll { get; set; }
-        public MyICommand GetValuesCriteria { get; set; }
+        public MyICommand GetRelatedValuesCriteria { get; set; }
         private GetRelatedValuesCommands commands = new GetRelatedValuesCommands();
 
         private List<DMSType> types = new List<DMSType>();
         private ObservableCollection<long> gids = new ObservableCollection<long>();
         private ObservableCollection<ModelCode> typeModelCodes = new ObservableCollection<ModelCode>();
         private ObservableCollection<PropertyView> listedProperties = new ObservableCollection<PropertyView>();
+        private ObservableCollection<ModelCode> properties = new ObservableCollection<ModelCode>();
 
         private ObservableCollection<ModelCode> references = new ObservableCollection<ModelCode>();
         private ModelCode selectedReference;
@@ -47,9 +48,39 @@ namespace MVVM3.ViewModel
 
             ClearProperties = new MyICommand(ClearPropertiesCollection);
             ResetAll = new MyICommand(ResetAllForm);
-            //GetValuesCriteria = new MyICommand(GetValuesFromNMSCriteria);
+
+            RemoveDMSTypesWithoutReferences();
+
+            GetRelatedValuesCriteria = new MyICommand(GetValuesFromNMSCriteria);
 
             ResetAllForm();
+        }
+
+        public void GetValuesFromNMSCriteria()
+        {
+            if (SelectedType == DMSType.MASK_TYPE)
+            {
+                Messenger.Default.Send(new StatusMessage("You didn't choose a DMS type!", "Firebrick"));
+                return;
+            }
+            if (SelectedGid == -1)
+            {
+                Messenger.Default.Send(new StatusMessage("You didn't choose a Global ID!", "Firebrick"));
+                return;
+            }
+            if(SelectedReferenceModelCode == 0)
+            {
+                Messenger.Default.Send(new StatusMessage("You must choose a valid reference!", "Firebrick"));
+                return;
+            }
+            if (SelectedModels.Count == 0)
+            {
+                Messenger.Default.Send(new StatusMessage("You must choose atleast one property!", "Firebrick"));
+                return;
+            }
+
+            Messenger.Default.Send(new StatusMessage("Executing query. Please wait...", "SteelBlue"));
+            //ListedProperties = commands.GetValues(selectedGid, SelectedModels.ToList());
         }
 
         private void ClearPropertiesCollection()
@@ -57,36 +88,91 @@ namespace MVVM3.ViewModel
             SelectedModels.Clear();
         }
 
+        private void RemoveDMSTypesWithoutReferences()
+        {
+            List<DMSType> types = MainWindowViewModel.modelResourcesDesc.AllDMSTypes.ToList();
+            types.Remove(DMSType.MASK_TYPE);
+
+            foreach (DMSType type in types)
+            {
+                int numberOfReferences = 0;
+                foreach (var mc in MainWindowViewModel.modelResourcesDesc.GetAllPropertyIds(type))
+                {
+                    if ((((long)mc & 0x0000000000000009) == 0x0000000000000009)
+                        || ((long)mc & 0x0000000000000019) == 0x0000000000000019)
+                    {
+                        numberOfReferences++;
+                    }
+                }
+
+                if (numberOfReferences == 0) // dms type has no references by schema
+                {
+                    Types.Remove(type);
+                }
+            }
+        }
+
         private void ResetAllForm()
         {
             SelectedModels.Clear();
-            SelectedType = (DMSType)1;
-            SelectedGid = -1;
             ListedProperties.Clear();
             References.Clear();
             TypeModelCodes.Clear();
+            Properties.Clear();
+            Gids.Clear();
+            SelectedType = (DMSType)1;
+            Gids = commands.GetGIDs(SelectedType);
             References = GetReferences();
+            SelectedGid = 0;
             SelectedTypeModelCode = 0;
+            SelectedReferenceModelCode = 0;
             Messenger.Default.Send(new StatusMessage("Criteria has been resetted.", "SteelBlue"));
         }
 
         private ObservableCollection<ModelCode> GetConcreteClassesModelCodes()
         {
-            if(SelectedTypeModelCode == 0)
+            if(((short)selectedReference) == 0)
             {
                 return new ObservableCollection<ModelCode>();
             }
 
-            ObservableCollection<ModelCode> availableTypeModelCodes = new ObservableCollection<ModelCode>() { };
-
-            List<ModelCode> selected_refs = new List<ModelCode>(References);
-
-            foreach(ModelCode reference in selected_refs)
+            ObservableCollection<ModelCode> availableTypeModelCodes = new ObservableCollection<ModelCode>
             {
-                availableTypeModelCodes.Add(MainWindowViewModel.modelResourcesDesc.GetModelCodeFromModelCodeName(reference.ToString().Split('_')[1]));
-            }
+                MainWindowViewModel.modelResourcesDesc.GetModelCodeFromModelCodeName(SelectedReferenceModelCode.ToString().Split('_')[1])
+            };
 
             return availableTypeModelCodes;
+        }
+
+        public ObservableCollection<ModelCode> Properties
+        {
+            get => properties;
+            set
+            {
+                if (properties != value)
+                {
+                    properties = value;
+                    OnPropertyChanged("Properties");
+                }
+            }
+        }
+
+        public System.Collections.IList SelectedItems
+        {
+            get
+            {
+                return SelectedModels;
+            }
+            set
+            {
+                foreach (ModelCode model in value)
+                {
+                    if (!SelectedModels.Contains(model))
+                    {
+                        SelectedModels.Add(model);
+                    }
+                }
+            }
         }
 
         private ObservableCollection<ModelCode> GetReferences()
@@ -114,6 +200,17 @@ namespace MVVM3.ViewModel
                     selectedReference = value;
                     TypeModelCodes.Clear();
                     TypeModelCodes = GetConcreteClassesModelCodes();
+
+                    try
+                    {
+                        // Populate properties for selected reference
+                        Properties = new ObservableCollection<ModelCode>(MainWindowViewModel.modelResourcesDesc.GetAllPropertyIds(TypeModelCodes[0]).ToList());
+                    }
+                    catch
+                    {
+                        Properties.Clear();
+                    }
+
                     OnPropertyChanged("SelectedReferenceModelCode");
                 }
             }
@@ -140,6 +237,14 @@ namespace MVVM3.ViewModel
                 if (value != selectedTypeModelCode)
                 {
                     selectedTypeModelCode = value;
+                    SelectedGid = -1;
+                    ListedProperties.Clear();
+                    References.Clear();
+                    TypeModelCodes.Clear();
+                    References = GetReferences();
+                    SelectedTypeModelCode = 0;
+                    Properties.Clear();
+                    SelectedReferenceModelCode = 0;
                     OnPropertyChanged("SelectedTypeModelCode");
                 }
             }
@@ -154,24 +259,6 @@ namespace MVVM3.ViewModel
                 {
                     listedProperties = value;
                     OnPropertyChanged("ListedProperties");
-                }
-            }
-        }
-
-        public System.Collections.IList SelectedItems
-        {
-            get
-            {
-                return SelectedModels;
-            }
-            set
-            {
-                foreach (ModelCode model in value)
-                {
-                    if (!SelectedModels.Contains(model))
-                    {
-                        SelectedModels.Add(model);
-                    }
                 }
             }
         }
@@ -198,7 +285,6 @@ namespace MVVM3.ViewModel
                 if (selectedType != value)
                 {
                     selectedType = value;
-                    OnPropertyChanged("SelectedType");
 
                     // Get all global ids from service via proxy
                     try
@@ -210,10 +296,8 @@ namespace MVVM3.ViewModel
                         SelectedModels.Clear();
                         SelectedGid = -1;
                         ListedProperties.Clear();
-                        SelectedReferenceModelCode = 0;
-
-
-                        // List all references
+                        Properties.Clear();
+                        References.Clear();
                         References = GetReferences();
                     }
                     catch
@@ -224,6 +308,9 @@ namespace MVVM3.ViewModel
                             SelectedGid = -1;
                         }
                     }
+
+                    OnPropertyChanged("SelectedType");
+                    OnPropertyChanged("References");
                 }
             }
         }
